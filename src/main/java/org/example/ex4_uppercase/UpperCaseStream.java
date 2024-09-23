bd0c90303144d42422e68674034a6e5cbed15c61
Maria.C <maria.cabulea22@gmail.com>
2024-09-23 20:58:42 +0300

    unit ex4

---
+++ src/main/java/org/example/ex4_uppercase/UpperCaseStream.java
@@ -0,0 +1,29 @@
+package org.example.ex4_uppercase;
+
+/*
+    Write a method that converts all strings in a list to their upper case using streams.
+    Input: ana are mere
+    Output: ANA ARE MERE
+
+    Metoda 1:
+    - stream
+    - toUppercase ?
+    - map
+
+    Metoda 2:
+    - for
+    - toUpperCase ?
+    - get element pe pozitia i, set cu upperCase
+
+    Metoda 3:
+    - extra, check the functionalities from String
+*/
+
+import java.util.List;
+
+public class UpperCaseStream {
+
+    public static void main(String[] args) {
+        List<String> text = List.of("ana", "are", "mere");
+    }
+}
